c6f69ea2311a4412dc8d85715ca624d5cf04f93a
mochan42 <mochan@students.42wolfsburg.de>
2023-04-04 00:13:14 +0200

    1st implementation of easyfind

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 19:41:02 by mochan            #+#    #+#             */
-/*   Updated: 2023/04/03 18:50:48 by mochan           ###   ########.fr       */
+/*   Updated: 2023/04/04 00:10:14 by moninechan       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,18 @@
 int main()
 {
 	std::vector<int>	v1(5, 42);
+	std::list<int>		lst1;
 	
-	easyfind(v1, 10);
+
+	lst1.push_back(2);
+	lst1.push_back(1);
+	lst1.push_back(3);
+	lst1.push_back(1);
+	lst1.push_back(4);
+	printElements(lst1);
+	unsigned long firstOccurence;
+	std::cout << "\n";
+	firstOccurence = easyfind(lst1, 1);
+	std::cout << "firstOccurence index (start at 0 for first place): " << firstOccurence << "\n";
 	return 0;
 }
