4dc3fe57be4f603f5aea254e082df60dbbeb4f12
mochan42 <mochan@students.42wolfsburg.de>
2023-04-05 16:15:33 +0200

    setup ex02

---
+++ ex02/inc/MutantStack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moninechan <moninechan@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 23:19:17 by moninechan        #+#    #+#             */
+/*   Updated: 2023/04/05 16:14:44 by moninechan       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include "MutantStack.h"
+#include <iostream>
+
+class MutantStack
+{
+    
+};
+
+
+
+#endif
